10c54a8f73286e1a7f7e6c0ba7ca4f1889fd7db6
George Kim <65160195+klinted@users.noreply.github.com>
2020-11-06 18:44:18 +0600

    Version 0.3.1
    Here app with multiple backStack, without shit

--- app/src/main/java/com/kodo/friple/mvvm/view/fragments/ChatsView.kt
+++ app/src/main/java/com/kodo/friple/mvvm/view/fragments/ChatsView.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.kodo.friple.R
 import com.kodo.friple.databinding.ChatsScreenBinding
